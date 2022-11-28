f827151d379a396f9e6ebb927ea17290ff7ca4ee
Eric Zheng <erzh@live.unc.edu>
2022-11-28 02:59:52 -0500

    created new js lib subdir, added api and constants files
    constants exports constant values utilized in other lib files and in
    the rest of the webapp
    
    api defines post() and get() functions for querying the backend
    database, in particular, requests are directed to the base url exported
    from constants.js

---
+++ src/lib/api.js
@@ -0,0 +1,33 @@
+//import { error } from '@sveltejs/kit';
+import { base } from './constants';
+
+// define an http api for frontend to query backend database
+async function send({ method, path, data, token }) {
+    const opts = { method, headers: {'Content-Type': 'application/json'} };
+
+    if (data) {
+        opts.body = JSON.stringify(data);
+
+        console.log('api', opts.body);
+    }
+
+    if (token) {
+        opts.headers['token'] = JSON.stringify(token);
+    }
+
+    const res = await fetch(`${base}/${path}`, opts);
+
+    // immediately returns the result of fetch
+    // leaves any error handling up to the caller
+    return res;
+}
+
+export function get(path, token) {
+	return send({ method: 'GET', path, token });
+}
+
+export function post({ path, data, token }) {
+	return send({ method: 'POST', path, data, token });
+}
+
+// PUT and DELETE not implemented
