172e9d1c9d7bd5e7bfc2a21cfddaa443f0420867
Eric Zheng <ezheng118@gmail.com>
2022-11-29 00:13:14 -0500

    Revert "changed adapter to try and fix faulty Vercel deployment"
    This reverts commit d01f4bcfb3a70d6c8ec5e229cba3d02f456a7cdd.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import node from "@sveltejs/adapter-auto";
-//import node from "@sveltejs/adapter-node";
+import node from "@sveltejs/adapter-node";
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
